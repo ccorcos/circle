ef3ebc6f72cdc52d20fe3bb88f00453c9d117d75
Chet Corcos <ccorcos@gmail.com>
2017-03-20 19:37:01 -0700

    full screen

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import p5 from "p5";
 
 // create the container with the canvas centered on the page
 const root = document.createElement("div");
+root.style.position = "absolute";
+root.style.top = 0;
+root.style.bottom = 0;
+root.style.left = 0;
+root.style.right = 0;
 root.style.display = "flex";
 root.style.justifyContent = "center";
 document.body.appendChild(root);
@@ -23,16 +28,17 @@ const random = (min, max) => Math.random() * (max - min) + min;
 // the main sketch
 var sketch = p => {
   // size of the canvas
-  const EDGE = Math.min(window.innerHeight, window.innerWidth) - 20;
-  const CENTER = EDGE / 2;
+  const HEIGHT = window.innerHeight;
+  const WIDTH = window.innerWidth;
+  const EDGE = Math.min(HEIGHT, WIDTH);
+  const CENTERX = WIDTH / 2;
+  const CENTERY = HEIGHT / 2;
   const RADIUS = EDGE / 6;
-  const WIDTH = RADIUS * 2;
-  const HEIGHT = RADIUS * 2;
   const FRACTION = 40;
   const OFFSET = p.TAU / FRACTION;
 
   p.setup = () => {
-    p.createCanvas(EDGE, EDGE);
+    p.createCanvas(WIDTH, HEIGHT);
   };
 
   let tick = 0.0;
@@ -48,33 +54,33 @@ var sketch = p => {
     p.stroke(255);
     p.strokeWeight(5);
 
-    // const speed = p.mouseX / EDGE * 0.1;
+    // const speed = p.mouseX / WIDTH * 0.1;
     const speed = random(0.01, 0.03);
 
     const x = p.mouseX;
-    // const x = Math.sin(tick) * EDGE;
-    const arms = Math.ceil(x / EDGE * (FRACTION / 2 - 1));
+    // const x = Math.sin(tick) * WIDTH;
+    const arms = Math.ceil(x / WIDTH * (FRACTION / 2));
 
     const y = p.mouseY;
-    const arc = y / EDGE * p.TAU;
+    const arc = y / HEIGHT * p.TAU;
 
     for (let i = 0; i < arms; i++) {
       p.push();
-      p.translate(CENTER, CENTER);
+      p.translate(CENTERX, CENTERY);
       p.rotate(i * OFFSET);
       p.rotate(Math.sin(tick / RPP[0] + START[0]) * RPP[0]);
-      p.arc(-RADIUS, 0, WIDTH, HEIGHT, 0, arc);
-      p.arc(+RADIUS, 0, WIDTH, HEIGHT, p.PI, p.PI + arc);
+      p.arc(-RADIUS, 0, RADIUS * 2, RADIUS * 2, 0, arc);
+      p.arc(+RADIUS, 0, RADIUS * 2, RADIUS * 2, p.PI, p.PI + arc);
       p.pop();
     }
 
     for (let i = 0; i < arms; i++) {
       p.push();
-      p.translate(CENTER, CENTER);
+      p.translate(CENTERX, CENTERY);
       p.rotate(i * OFFSET);
       p.rotate(Math.sin(tick / RPP[1] + START[1]) * RPP[1]);
-      p.arc(-RADIUS, 0, WIDTH, HEIGHT, -arc, 0);
-      p.arc(+RADIUS, 0, WIDTH, HEIGHT, p.PI - arc, p.PI);
+      p.arc(-RADIUS, 0, RADIUS * 2, RADIUS * 2, -arc, 0);
+      p.arc(+RADIUS, 0, RADIUS * 2, RADIUS * 2, p.PI - arc, p.PI);
       p.pop();
     }
     tick += speed;
