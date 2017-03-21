7963b1fc6ec828a863fd37805f1cdd3b9c205796
Chet Corcos <ccorcos@gmail.com>
2017-03-20 18:36:55 -0700

    mouse for speed

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ root.style.display = "flex";
 root.style.justifyContent = "center";
 document.body.appendChild(root);
 
-// size of the canvas
-const EDGE = Math.min(window.innerHeight, window.innerWidth) - 20;
-
 // helper functions
 const range = (min, max, step) =>
   Array(Math.round((max - min) / step)).fill(0).map((_, i) => i * step + min);
@@ -21,27 +18,56 @@ const map = fn => list => list.map(fn);
 
 // the main sketch
 var sketch = p => {
+  // size of the canvas
+  const EDGE = Math.min(window.innerHeight, window.innerWidth) - 20;
+  const CENTER = EDGE / 2;
+  const RADIUS = EDGE / 6;
+  const WIDTH = RADIUS * 2;
+  const HEIGHT = RADIUS * 2;
+  const ARC = p.TAU / 3;
+  const OFFSET = p.TAU / 40;
+  const ARMS = 7;
+  const SPEED = {
+    MIN: 0.0,
+    MAX: 0.1
+  };
+
   p.setup = () => {
     p.createCanvas(EDGE, EDGE);
   };
+
+  let tick = 0.0;
 
   p.draw = () => {
     p.background(51);
     p.noFill();
     p.stroke(255);
     p.strokeWeight(5);
-    // p.curve(5, 26, 5, 26, 73, 24, 73, 61);
 
-    const points = pipe(range(0, p.TAU, 0.02), [
-      // one cycle of a sine wave
-      map(i => ({ x: i, y: Math.sin(i) })),
-      // scale and move it to the middle
-      map(({ x, y }) => ({ x: x / p.TAU * EDGE, y: y * EDGE / 8 + EDGE / 2 }))
-    ]);
+    const x = p.mouseX / EDGE - 0.5;
+    const speed = (SPEED.MAX - SPEED.MIN) * x + SPEED.MIN;
 
-    p.beginShape();
-    points.forEach(({ x, y }) => p.curveVertex(x, y));
-    p.endShape();
+    for (let i = 0; i < ARMS; i++) {
+      p.push();
+      p.translate(CENTER, CENTER);
+      p.rotate(i * OFFSET);
+      p.rotate(tick + speed);
+      p.arc(-RADIUS, 0, WIDTH, HEIGHT, 0, ARC);
+      p.arc(+RADIUS, 0, WIDTH, HEIGHT, p.PI, p.PI + ARC);
+      p.pop();
+    }
+
+    for (let i = 0; i < ARMS; i++) {
+      p.push();
+      p.translate(CENTER, CENTER);
+      p.rotate(i * OFFSET);
+      p.rotate(-tick - speed);
+
+      p.arc(-RADIUS, 0, WIDTH, HEIGHT, -ARC, 0);
+      p.arc(+RADIUS, 0, WIDTH, HEIGHT, p.PI - ARC, p.PI);
+      p.pop();
+    }
+    tick += speed;
   };
 };
 
