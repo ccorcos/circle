bafd7a2f266a78a980c6bec2b9c6353a05345b82
Chet Corcos <ccorcos@gmail.com>
2017-03-20 17:59:57 -0700

    sine wave doesnt look right

--- src/index.js
+++ src/index.js
@@ -1,22 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { css } from 'glamor'
+import p5 from "p5";
 
-const style = css({
-  color: 'blue'
-})
+// create the container with the canvas centered on the page
+const root = document.createElement("div");
+root.style.display = "flex";
+root.style.justifyContent = "center";
+document.body.appendChild(root);
 
-class Index extends React.PureComponent {
-  render() {
-    return (
-      <div className={style}>
-        Hello World!
-      </div>
-    )
-  }
-}
+// size of the canvas
+const EDGE = Math.min(window.innerHeight, window.innerWidth) - 20;
 
-const root = document.createElement('div')
-document.body.appendChild(root)
+// helper functions
+const range = (min, max, step) =>
+  Array(Math.round((max - min) / step)).fill(0).map((_, i) => i * step + min);
 
-ReactDOM.render(<Index/>, root)
+const flatten = list => list.reduce((acc, item) => acc.concat(item), []);
+
+const pipe = (init, fns) => fns.reduce((value, fn) => fn(value), init);
+
+const map = fn => list => list.map(fn);
+
+// the main sketch
+var sketch = p => {
+  p.setup = () => {
+    p.createCanvas(EDGE, EDGE);
+  };
+
+  p.draw = () => {
+    p.background(51);
+    p.noFill();
+    p.stroke(255);
+    p.strokeWeight(5);
+    // p.curve(5, 26, 5, 26, 73, 24, 73, 61);
+
+    const points = pipe(range(0, p.TAU, 0.02), [
+      // one cycle of a sine wave
+      map(i => ({ x: i, y: Math.sin(i) })),
+      // scale and move it to the middle
+      map(({ x, y }) => ({ x: x / p.TAU * EDGE, y: y * EDGE / 8 + EDGE / 2 }))
+    ]);
+
+    p.beginShape();
+    points.forEach(({ x, y }) => p.curveVertex(x, y));
+    p.endShape();
+  };
+};
+
+const fun = new p5(sketch, root);
