7b9c8be8b520a18c55e23404a1d6a290c264cb1f
Chet Corcos <ccorcos@gmail.com>
2017-03-29 14:12:47 -0700

    favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+const prod = process.argv.indexOf("-p") !== -1;
 
 module.exports = {
   entry: {
@@ -32,6 +34,9 @@ module.exports = {
     ]
   },
   plugins: [
+    prod
+      ? new FaviconsWebpackPlugin(path.join(__dirname, "assets/logo.png"))
+      : () => {},
     new HtmlWebpackPlugin({
       chunks: ["index"],
       template: path.join(__dirname, "src/index.html")
