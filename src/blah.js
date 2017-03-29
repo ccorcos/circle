da27e56ea6cf39c8b89fa016d0b1ce8f3300dba7
Chet Corcos <ccorcos@gmail.com>
2017-03-28 20:46:46 -0700

    resizing

--- src/blah.js
+++ src/blah.js
@@ -5,6 +5,31 @@ import { css } from "glamor";
 import * as u from "./utils";
 import songUrl from "../assets/zedd-stay.m4a";
 
+// some FFT documentation here:
+// https://p5js.org/reference/#/p5.FFT
+// https://p5js.org/reference/#/p5.FFT/getEnergy
+
+const styles = {
+  layout: css({
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    display: "flex",
+    flexDirection: "row"
+  }),
+  content: css({
+    flex: "1",
+    backgroundColor: "white"
+  }),
+  toolbar: css({
+    width: 150,
+    // backgroundColor: "rgb(51, 51, 51)"
+    backgroundColor: "black"
+  })
+};
+
 export default class Sketch extends React.PureComponent {
   constructor(props) {
     super(props);
@@ -13,25 +38,37 @@ export default class Sketch extends React.PureComponent {
     this.root = node;
   };
   render() {
-    return <div ref={this.saveRoot} />;
+    return (
+      <div className={styles.layout}>
+        <div className={styles.content} ref={this.saveRoot} />
+        <div className={styles.toolbar} />
+      </div>
+    );
   }
   componentDidMount() {
     new p5(this.sketch, this.root);
+    window.onresize = this.onResize;
   }
-  sketch = p => {
-    // canvas size
-    const HEIGHT = window.innerHeight;
-    const WIDTH = window.innerWidth;
+  deriveSizes() {
+    // size of the canvas
+    const rect = this.root.getBoundingClientRect();
+    this.HEIGHT = rect.height;
+    this.WIDTH = rect.width;
     // edge of a square in the middle
-    const EDGE = Math.min(HEIGHT, WIDTH);
-    const CENTERX = WIDTH / 2;
-    const CENTERY = HEIGHT / 2;
+    this.EDGE = Math.min(this.HEIGHT, this.WIDTH);
+    this.CENTERX = this.WIDTH / 2;
+    this.CENTERY = this.HEIGHT / 2;
+    this.RADIUS = this.EDGE / 3;
+  }
+  onResize = () => {
+    if (this.canvas) {
+      this.deriveSizes();
+      this.canvas.resize(this.WIDTH, this.HEIGHT);
+    }
+  };
+  sketch = p => {
+    this.deriveSizes();
 
-    // some FFT documentation here:
-    // https://p5js.org/reference/#/p5.FFT
-    const FMAX = 44100 / 2;
-    // we get 1024 frequency bins back from the FFT
-    const BINS = 1024;
     // A0 is 27.5Hz which is below C1
     const FSTART = 27.5;
     // 7 octaves on a piano, lets use 8
@@ -46,28 +83,26 @@ export default class Sketch extends React.PureComponent {
       });
     });
 
-    let mic, fft, song;
-
     p.preload = () => {
-      song = p.loadSound(songUrl);
+      this.song = p.loadSound(songUrl);
     };
 
     p.setup = () => {
-      p.createCanvas(WIDTH, HEIGHT);
+      this.canvas = p.createCanvas(this.WIDTH, this.HEIGHT);
       p.noFill();
 
       // using microphone
-      // mic = new p5.AudioIn();
-      // mic.start();
-      // fft = new p5.FFT();
-      // fft.setInput(mic);
+      // this.mic = new p5.AudioIn();
+      // this.mic.start();
+      // this.fft = new p5.FFT();
+      // this.fft.setInput(this.mic);
 
       // using a song file
-      song.setVolume(1.0);
-      song.playMode("restart");
-      song.play();
-      fft = new p5.FFT();
-      fft.setInput(song);
+      this.song.setVolume(1.0);
+      this.song.playMode("restart");
+      this.song.play();
+      this.fft = new p5.FFT();
+      this.fft.setInput(this.song);
     };
 
     // hue offset
@@ -81,25 +116,24 @@ export default class Sketch extends React.PureComponent {
 
       // inner and outer radius of the circle
       // exponentiate amplitude to give more shape to the circles
-      const RADIUS = EDGE / 3;
+
       let INNER, SHAPE;
       // INNER = p.map(p.mouseY, 0, HEIGHT, 0, EDGE / 6);
       // SHAPE = p.map(p.mouseX, 0, WIDTH, 1, 10);
-      INNER = EDGE / 8;
+      INNER = this.EDGE / 8;
       SHAPE = 3;
 
       // this returns an array of 1024 values but we're going to use getEnergy instead:
-      // https://p5js.org/reference/#/p5.FFT/getEnergy
-      fft.analyze();
+      this.fft.analyze();
 
       const drawVertex = (freq, i) => {
-        const radius = Math.pow(fft.getEnergy(freq) / 255, SHAPE) *
-          (RADIUS - INNER) +
+        const radius = Math.pow(this.fft.getEnergy(freq) / 255, SHAPE) *
+          (this.RADIUS - INNER) +
           INNER;
         const angle = i / STEPS * p.TAU;
         p.vertex(
-          CENTERX + radius * Math.cos(angle),
-          CENTERY + radius * Math.sin(angle)
+          this.CENTERX + radius * Math.cos(angle),
+          this.CENTERY + radius * Math.sin(angle)
         );
       };
 
@@ -147,10 +181,10 @@ export default class Sketch extends React.PureComponent {
         ].forEach((letter, i) => {
           const angle = i / 12 * p.TAU;
           p.line(
-            CENTERX,
-            CENTERY,
-            CENTERX + RADIUS * Math.cos(angle),
-            CENTERY + RADIUS * Math.sin(angle)
+            this.CENTERX,
+            this.CENTERY,
+            this.CENTERX + this.RADIUS * Math.cos(angle),
+            this.CENTERY + this.RADIUS * Math.sin(angle)
           );
 
           p.textSize(14);
@@ -158,8 +192,8 @@ export default class Sketch extends React.PureComponent {
           p.fill(255, 255, 255, 255 * 0.2);
           p.text(
             letter,
-            CENTERX + RADIUS * 1.1 * Math.cos(angle),
-            CENTERY + RADIUS * 1.1 * Math.sin(angle)
+            this.CENTERX + this.RADIUS * 1.1 * Math.cos(angle),
+            this.CENTERY + this.RADIUS * 1.1 * Math.sin(angle)
           );
         });
       }
