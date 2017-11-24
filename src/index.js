1de175c275cebecd0f894a5336ca444abce7abd7
Chet Corcos <ccorcos@gmail.com>
2017-11-24 05:22:43 -0800

    deploy

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Sketch from "./sketch";
-
-const root = document.createElement("div");
-document.body.appendChild(root);
-
-ReactDOM.render(<Sketch />, root);
